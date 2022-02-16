fd593e164b70a4954c928850f554b8ec13ce5264
Nadzeya Kaliachka <nadzeyakaliachka@Nadzeyas-MBP.attlocal.net>
2022-02-16 01:52:43 -0800

    Refactoring + WebSite field url type test

--- tests/ContactFormTest.js
+++ tests/ContactFormTest.js
@@ -4,20 +4,31 @@ Feature('Contact Page');
 
 let assert = require('assert')
 
+// BeforeSuite(async ({ I }) => {
+//     await I.amOnPage('/' + contactPage.contactPageUrl);
+//     await I.seeElement(headerFragment.siteTitleLink);
+// });
+
 Before(async ({ I }) => {
     await I.amOnPage('/' + contactPage.contactPageUrl);
-    await I.seeElement(headerFragment.siteTitleLink);
     await I.seeElement(contactPage.contactForm);
 });
 
 Scenario('Test Contact Page tab is active', async ({I}) => {
-    let ariaCurrent = await I.grabAttributeFrom(contactPage.contactPageMenuHeaderLink, 'aria-current');
-    assert.equal(ariaCurrent, 'page');
+    assert.equal(await I.getNavigationItemActivityStatus(contactPage.contactPageMenuHeaderLink), 'page');
 });
 
-Scenario('Test Name input field is required', async ({I}) => {
-    await I.seeElement(contactPage.contactFormFields.nameFieldLabel.find('span').withText('(required)'));
-    await I.seeElement(contactPage.contactFormFields.nameInput.withAttr({required:''}));
-    let ariaRequired = await I.grabAttributeFrom(contactPage.contactFormFields.nameInput, 'aria-required');
-    assert.equal(ariaRequired, 'true');
-})
+Scenario('Test Contact Form mandatory fields', async ({I}) => {
+    assert.equal(await contactPage.checkContactFormRequiredFields(), true);
+});
+
+Scenario('Test Website input field is required to be URL', async ({I}) => {
+    assert.equal(await I.getTypeOfElement(contactPage.contactFormFields.webSiteInput),'url');
+});
+
+
+
+
+
+
+
